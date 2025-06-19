b75898235811aac53375b98b839aee2c07228706
Mila Frerichs <mila.frerichs@gmail.com>
2025-06-19 12:47:48 +0200

    fix: add migrate to ci workflow; help pg with migration

--- db/migrate/20250619082743_change_climb_logs_grade.rb
+++ db/migrate/20250619082743_change_climb_logs_grade.rb
@@ -2,7 +2,7 @@ class ChangeClimbLogsGrade < ActiveRecord::Migration[8.0]
   def change
     reversible do |direction|
       change_table :climb_logs do |t|
-        direction.up   { t.change :grade, :decimal }
+        direction.up   { t.change :grade, 'decimal(4,2) USING (grade::numeric)' }
         direction.down { t.change :grade, :string }
       end
     end

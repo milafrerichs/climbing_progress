47278942fba43d83f46fd6d09e95f74edd469b08
Mila Frerichs <mila.frerichs@gmail.com>
2025-08-22 06:58:40 +0200

    chore: ux improvements
    some formatting

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,8 +1,9 @@
-# Pin npm packages by running ./bin/importmap
+# Pin npm packages by running ./bin/importmap 
 
-pin "application"
-pin "alpinejs" # @3.14.9
-pin_all_from "app/javascript/alpine", under: "alpine", preload: false
-pin_all_from "app/javascript/climb_logs", under: "climb_logs", preload: false
+pin "application" 
+pin "alpinejs" #@3.14.9 
+pin_all_from "app/javascript/alpine", under: "alpine", preload: false 
+pin_all_from "app/javascript/climb_logs", under: "climb_logs", preload: false 
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels", preload: true
+pin "@tailwindplus/elements", to: "@tailwindplus--elements.js" # @1.0.9
